a50f448a71c087c70cf7c23d1290e63349aa6240
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-05-20 17:45:31 -0700

    feature: create sql file with postgresql triggers for update follower and following counting

--- src/main/resources/db/migrations/V2_CREATE_PROFILE_TABLE_TRIGGER.sql
+++ src/main/resources/db/migrations/V2_CREATE_PROFILE_TABLE_TRIGGER.sql
@@ -4,16 +4,35 @@ CREATE OR REPLACE FUNCTION increase_profile_followers_acount()
   AS
 $$
 BEGIN
-	IF NEW.last_name <> OLD.last_name THEN
-		 INSERT INTO employee_audits(employee_id,last_name,changed_on)
-		 VALUES(OLD.id,OLD.last_name,now());
-	END IF;
-
+	UPDATE 
+		TB_PROFILE 
+	SET 
+		TB_PROFILE.profile_followers_count = TB_PROFILE.profile_followers_count + 1
+	WHERE
+		TB_PROFILE.profile_id = NEW.profile_id;
 	RETURN NEW;
 END;
 $$
 
-CREATE TRIGGER profile_followers_acount_trigger AFTER INSERT ON TB_PROFILE_FOLLOWER FOR EACH ROW EXECUTE PROCEDURE increase_profile_followers_acount();
+CREATE TRIGGER increase_profile_followers_acount_trigger AFTER INSERT ON TB_PROFILE_FOLLOWER FOR EACH ROW EXECUTE PROCEDURE increase_profile_followers_acount();
+
+CREATE OR REPLACE FUNCTION decrease_profile_followers_acount()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+	UPDATE 
+		TB_PROFILE 
+	SET 
+		TB_PROFILE.profile_followers_count = TB_PROFILE.profile_followers_count - 1
+	WHERE
+		TB_PROFILE.profile_id = NEW.profile_id;
+	RETURN NEW;
+END;
+$$
+
+CREATE TRIGGER decrease_profile_followers_acount_trigger AFTER DELETE ON TB_PROFILE_FOLLOWER FOR EACH ROW EXECUTE PROCEDURE decrease_profile_followers_acount();
 
 CREATE OR REPLACE FUNCTION increase_profile_followings_acount()
   RETURNS TRIGGER 
@@ -21,13 +40,32 @@ CREATE OR REPLACE FUNCTION increase_profile_followings_acount()
   AS
 $$
 BEGIN
-	IF NEW.last_name <> OLD.last_name THEN
-		 INSERT INTO employee_audits(employee_id,last_name,changed_on)
-		 VALUES(OLD.id,OLD.last_name,now());
-	END IF;
-
+	UPDATE 
+		TB_PROFILE 
+	SET 
+		TB_PROFILE.profile_following_count = TB_PROFILE.profile_following_count + 1
+	WHERE
+		TB_PROFILE.profile_id = NEW.profile_id;
 	RETURN NEW;
 END;
 $$
 
-CREATE TRIGGER profile_followings_acount AFTER INSERT ON TB_PROFILE_FOLLOWING FOR EACH ROW EXECUTE PROCEDURE increase_profile_followings_acount();
+CREATE TRIGGER increase_profile_followings_acount AFTER INSERT ON TB_PROFILE_FOLLOWING FOR EACH ROW EXECUTE PROCEDURE increase_profile_followings_acount();
+
+CREATE OR REPLACE FUNCTION decrease_profile_followings_acount()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+	UPDATE 
+		TB_PROFILE 
+	SET 
+		TB_PROFILE.profile_following_count = TB_PROFILE.profile_following_count - 1
+	WHERE
+		TB_PROFILE.profile_id = NEW.profile_id;
+	RETURN NEW;
+END;
+$$
+
+CREATE TRIGGER decrease_profile_followings_acount AFTER INSERT ON TB_PROFILE_FOLLOWING FOR EACH ROW EXECUTE PROCEDURE decrease_profile_followings_acount();
